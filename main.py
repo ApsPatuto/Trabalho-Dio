c95c64e2534ec6641002ae612d55b350cb339dee
ApsPatuto <ceifador75vitoria@gmail.com>
2024-09-17 16:08:01 -0300

    Trabalho feito

--- main.py
+++ main.py
@@ -12,13 +12,13 @@
 #Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
 
 #Se XP for menor do que 1.000 = Ferro
-#Se XP for entre 1.001 e 2.000 = Bronze
-#Se XP for entre 2.001 e 5.000 = Prata
-#Se XP for entre 5.001 e 7.000 = Ouro
-#Se XP for entre 7.001 e 8.000 = Platina
-#Se XP for entre 8.001 e 9.000 = Ascendente
-#Se XP for entre 9.001 e 10.000= Imortal
-#Se XP for maior ou igual a 10.001 = Radiante
+#Se XP for entre 1.000 e 1.999 = Bronze
+#Se XP for entre 2.000 e 4.999 = Prata
+#Se XP for entre 5.000 e 6.999 = Ouro
+#Se XP for entre 7.000 e 7.999 = Platina
+#Se XP for entre 8.000 e 8.999 = Ascendente
+#Se XP for entre 9.000 e 9.999 = Imortal
+#Se XP for maior ou igual a 10.000 = Radiante
 
 ## Saída
 
@@ -26,11 +26,29 @@
 #"O Herói de nome **{nome}** está no nível de **{nivel}**"
 
 heroi = [
-    ["Marcos", 1500],
-    ["Richard", 4385],
-    ["Alfred", 100000000]
+    ["Marcos", 15000],
 ]
+#    ["Richard", 4385],
+#    ["Alfred", 100000000]
+while True:
+    if (heroi[0][1]) < 1000:
+        nivel_heroi = "Ferro"
+    elif (heroi[0][1]) >= 1000 and (heroi[0][1]) < 2000:
+        nivel_heroi = "Bronze"
+    elif (heroi[0][1]) >= 2000 and (heroi[0][1]) < 5000:
+        nivel_heroi = "Prata"
+    elif (heroi[0][1]) >= 5000 and (heroi[0][1]) < 7000:
+        nivel_heroi = "Ouro"
+    elif (heroi[0][1]) >= 7000 and (heroi[0][1]) < 8000:
+        nivel_heroi = "Platina"
+    elif (heroi[0][1]) >= 8000 and (heroi[0][1]) < 9000:
+        nivel_heroi = "Ascendente"
+    elif (heroi[0][1]) >= 9000 and (heroi[0][1]) < 10000:
+        nivel_heroi = "Imortal"
+    else:
+        nivel_heroi = "Radiante"
+    break
 
-verificador = input("Deseja verificar o nível de seus herois? \nS para sim \nN para não \nS/N? ")
-#if verificador = "S" or "s":
-#    hherois
+verificar = str(input("Deseja verificar o nível de seu heroi? \nS para sim \nN para não \nS/N? "))
+if verificar == "S" or "s":
+ print(f"O herói de nome {heroi[0][0]} está no nível de {nivel_heroi}")
